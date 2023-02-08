2a56b488fcf2e86fef8ce7b977c2b83b5d93707d
Waminms <67516571+Waminms@users.noreply.github.com>
2023-02-08 18:31:13 +0200

    Added methods fillUp(), sort(), save()

--- src/Dictionary.java
+++ src/Dictionary.java
@@ -1,7 +1,6 @@
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Dictionary {
     private Map<String, String> dictionary = new HashMap<>();
@@ -12,6 +11,10 @@ public class Dictionary {
         dictionary.put("in", "в");
         dictionary.put("my", "мій");
         dictionary.put("shoes", "черевики");
+    }
+
+    public Dictionary(Map<String, String> dictionary) {
+        this.dictionary = dictionary;
     }
 
     public Map<String, String> getDictionary() {
@@ -37,10 +40,44 @@ public class Dictionary {
         System.out.println("Translation saved in UkrainianOut.txt ");
     }
 
+    public void fillUp() {
+        Scanner sc = new Scanner(System.in);
+        String entry = "exit";
+        while (!entry.equalsIgnoreCase("s")) {
+            System.out.println("Please, enter new entry: ");
+            System.out.print("Eng: ");
+            String eng = sc.next();
+            System.out.print("Ukr: ");
+            String ukr = sc.next();
+            dictionary.put(eng, ukr);
+            System.out.println("Add one more entry, press: Y");
+            System.out.println("Save updated dictionary, press: S");
+            entry = sc.next();
+        }
+    }
+
+    public String sort() {
+        StringBuilder dictionarySorted = new StringBuilder();
+        Set<Map.Entry<String, String>> set = dictionary.entrySet();
+        for(Map.Entry<String, String> s : set) {
+            dictionarySorted.append(s.getKey()).append(": " + "\t").append(s.getValue()).append("\n");
+        }
+        return dictionarySorted.toString();
+    }
+
+    public void save() {
+        String dictionaryToSave = sort();
+        try (PrintWriter pw = new PrintWriter("/Users/ac/IdeaProjects/Translater/src/Dictionary.txt")){
+            pw.println(dictionaryToSave);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Dictionary saved in Dictionary.txt ");
+
+    }
+
     @Override
     public String toString() {
-        return "Dictionary{" +
-                "dictionary=" + dictionary +
-                '}';
+        return "Dictionary:\n" + sort();
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,11 @@ public class Main {
     public static void main(String[] args) {
         Dictionary myDictionary = new Dictionary();
         myDictionary.translate(Input.readFile());
+        System.out.println(myDictionary);
+
+        myDictionary.fillUp();
+        System.out.println(myDictionary);
+        myDictionary.save();
+
     }
 }
